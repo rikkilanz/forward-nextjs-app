81af857a710b362dc665ae2e3467f83ae68dc87e
davekoo1106 <114951120+davekoo1106@users.noreply.github.com>
2023-09-22 22:29:25 -0700

    Hero Section

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
+import heroImage from '../images/hero-banner.jpg'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-end justify-between p-5">
-      <div className='flex'>
+    <main className="flex min-h-screen flex-col p-5">
+      <div className='flex flex-col items-end'>
         <div className='flex gap-5'>
           <Link href="/">Home</Link>
           <Link href="/">About</Link>
@@ -17,6 +18,40 @@ export default function Home() {
         </div>
       </div>
 
+    <div className='flex justify-around pt-10'>
+
+      <div className='flex flex-col justify-center'>
+
+        <div className='flex items-center'>
+          <div className='flex flex-col justify-center'>
+            <h1 className='text-6xl font-bold'>Dedicated Mentors to<br/>keep you moving <br/>Forward.</h1>
+            <p className='pt-4'>Built by mentees, for mentors & peers. Forward helps local tech industry<br/>professionals and new comers build communities and connections with<br/>each other 
+            </p>
+          </div>
+        </div>
+
+        <div className='flex flex-row gap-4 pt-4'>
+          <Link className='text-white bg-green-500 p-3 text-[13px] flex items-center' href="/">Get Started</Link>
+          <Link className='text-green-500 bg-white p-3 border-2 border-green-500 text-[13px] flex items-center ' href="/">Learn More</Link>
+        </div>
+
+      </div>
+
+      <div className='flex flex-row'>
+
+        <div>
+          <Image
+            width={700}
+            height={700}
+            src={heroImage}
+          />
+        </div>
+
+      </div>
+
+    </div>
+      
     </main>
-  )
+  )       
 }
+ 
